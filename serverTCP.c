934a839086973146673f408e78bbf567838c35b2
Roman <claprood.8@osu.edu>
2015-09-29 23:55:18 -0400

    trying

--- serverTCP.c
+++ serverTCP.c
@@ -6,6 +6,8 @@
 #include <signal.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
 
 /*Author: Roman Claprood and Eric Olson
@@ -21,7 +23,7 @@ int main (int argc, char *argv[])
 	int sock, newsock,portNum, pid;
 	socklen_t clientLen;
 	struct sockaddr_in server, client;
-	char request[256];
+	char request[1024];
 
 	if (argc < 2)
 	{
@@ -65,6 +67,7 @@ int main (int argc, char *argv[])
 		}
 	}
 	close(newsock);
+	close(sock);
 	return 0;
 }
 
@@ -72,9 +75,9 @@ int main (int argc, char *argv[])
 void sendBack(int newsock){
 
 	int nBytes = 0;
-	char message[256];
-	bzero(message,256);
-	nBytes = read(newsock,message,255);
+	char message[1024];
+	bzero(message,1024);
+	nBytes = read(newsock,message,1023);
    	if (nBytes < 0) 
 		printf("ERROR reading from socket");	
 	
@@ -85,16 +88,19 @@ void sendBack(int newsock){
 	
    	if (nBytes < 0) 
 		printf("ERROR writing to socket");
-	bzero(message,256);
+	bzero(message,1023);
+	close(newsock);
 }
 
 void parseAndSendResponse(int newsock,char request[]){
-	char *reqType = "GET";
 	int nBytes =0;
 	int correct =0;
 	int i =0;
-	char requestCopy[256];
-	char *strArray[40];
+	char requestCopy[1024];
+	char *strArray[100];
+	char responseHTTP[1024];
+	
+	//FILE *fname = "/index.html";
 	//Copy of string, in case I need the original
 	strcpy((char *)requestCopy,(char *)request);
 	char *tokens = strtok(requestCopy, " ,;\n\r");
@@ -102,14 +108,30 @@ void parseAndSendResponse(int newsock,char request[]){
 	while(tokens != NULL){
 		strArray[i] = malloc(strlen(tokens)+1);
 		strcpy(strArray[i],tokens);
-		printf("\n%s",strArray[i]);
-		i = i+1;
+		//printf("\n%s",strArray[i]);
+		i++;
 		tokens = strtok(NULL, " ,;\n\r");
 	}
+	int numElements =i;
 	
-	correct = strcmp(strArray[0],"GET");
-	if(correct != 0){
-		printf("Request type is not supported");
-	}
+	i =0;
+	while(i < numElements){		
+		if(strcmp(strArray[i],"HTTP/1.1")==0){
+			strcat(responseHTTP,"HTTP/1.1");
+			i++;
+		}
 
+		if(strcmp(strArray[i],"/index.html") ==0){
+			if(access("./index.html", F_OK ) != -1 ){
+    				strcat(responseHTTP," 200 OK");
+			} else {
+   				strcat(responseHTTP," 404 NOT FOUND");
+			}
+		strcat(responseHTTP,"\r\n");
+		}
+		
+		i++;
+	}
+	//strcat(responseHTTP,"\r\n");
+	printf("%s",responseHTTP);
 }
